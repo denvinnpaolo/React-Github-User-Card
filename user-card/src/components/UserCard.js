c436c3e39e3f63a0756ad3d906f38d24beabab18
Denvinn Magsino <lambda_school_loaner_170@Lambda-School-Loaner-170s-MacBook-Pro.local>
2019-09-06 22:30:23 -0700

    Added semantic-ui-react card with user info mounted in the card

--- user-card/src/components/UserCard.js
+++ user-card/src/components/UserCard.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function UserCard(props){
+const UserCard=(props)=>{
     console.log(props)
     return(
-        <div className="user">
-            <h1>{props.user.name}</h1>
-        </div>
+      <Card>
+      <Image src={props.user.avatar_url} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{props.user.name}</Card.Header>
+        <Card.Meta>{`Joined in ${props.user.created_at}`}</Card.Meta>
+        <Card.Description>
+         <p>{props.user.bio}</p>
+         <p>{props.user.location}</p>
+         <p>{`${props.user.followers} Followers`}</p>
+         <p>{`${props.user.following} Following`}</p>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          <a href={props.user.html_url}>Github Profile</a>
+        </a>
+      </Card.Content>
+    </Card>
     )
 }
+
+export default UserCard;
